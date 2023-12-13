f60ecbb465f3fa9dc60b02edf573672db6f7206d
Daniel J Wilson <daniel.j.wilson@gmail.com>
2023-12-13 10:18:07 -0500

    Updated Knights

--- 1_Knowledge/1_Project/knights/puzzle.py
+++ 1_Knowledge/1_Project/knights/puzzle.py
@@ -9,25 +9,47 @@ BKnave = Symbol("B is a Knave")
 CKnight = Symbol("C is a Knight")
 CKnave = Symbol("C is a Knave")
 
+
 # Puzzle 0
 # A says "I am both a knight and a knave."
 knowledge0 = And(
-    (AKnight),
-    (AKnave)
+    # A is a knight or a knave but not both
+    And(Or(AKnight, AKnave), Not(And(AKnight, AKnave))),
+    # If A is a knight his sentence is true:
+    Implication(AKnight, And(AKnight, AKnave)),
+    # If A is a knave his sentence is false:
+    Implication(AKnave, Not(And(AKnight, AKnave)))
 )
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
 knowledge1 = And(
-    # A says And(AKnave, BKnave)
+    # A is a knight or a knave but not both
+    And(Or(AKnight, AKnave), Not(And(AKnight, AKnave))),
+    And(Or(BKnight, BKnave), Not(And(BKnight, BKnave))),
+    # If A knight his sentence is true:
+    Implication(AKnight, And(AKnave, BKnave)),
+    # If A is a knave his sentence is false:
+    Implication(AKnave, Not(And(AKnave, BKnave)))
+
 )
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
 knowledge2 = And(
-    # TODO
+    # A and B are a knight or a knave but not both
+    And(Or(AKnight, AKnave), Not(And(AKnight, AKnave))),
+    And(Or(BKnight, BKnave), Not(And(BKnight, BKnave))),
+    # A says we are the same kind...
+    Implication(AKnight, Or(And(AKnight, BKnight), And(AKnave, BKnave))),
+    Implication(AKnave, Not(Or(And(AKnight, BKnight), And(AKnave, BKnave)))),
+    # B says "we are of different kinds"
+    # If BKnight, statement is True
+    Implication(BKnight, Or(And(AKnight, BKnave), And(AKnave, BKnight))),
+    # If BKnave, statement is False
+    Implication(BKnave, Not(Or(And(AKnight, BKnave), And(AKnave, BKnight))))
 )
 
 # Puzzle 3
@@ -44,8 +66,8 @@ def main():
     symbols = [AKnight, AKnave, BKnight, BKnave, CKnight, CKnave]
     puzzles = [
         ("Puzzle 0", knowledge0),
-        # ("Puzzle 1", knowledge1),
-        # ("Puzzle 2", knowledge2),
+        ("Puzzle 1", knowledge1),
+        ("Puzzle 2", knowledge2)
         # ("Puzzle 3", knowledge3)
     ]
     for puzzle, knowledge in puzzles:
